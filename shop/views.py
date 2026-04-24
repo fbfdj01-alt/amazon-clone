a3c8373f4f965a75ceceea403831dcc33dc5ab16
fbfdj01-alt <fbfdj01@gmail.com>
2026-04-24 16:35:03 +0300

    إضافة أزرار التعديل والحذف

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
 from .forms import ProductForm
 
@@ -14,4 +14,18 @@ def add_product(request):
             return redirect('product_list')
     else:
         form = ProductForm()
+    return render(request, 'shop/add_product.html', {'form': form})
+def delete_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    product.delete()
+    return redirect('product_list')
+def edit_product(request, pk):
+    product = get_object_or_404(Product, id=pk)
+    if request.method == "POST":
+        form = ProductForm(request.POST, instance=produ
+        if form.is_valid():
+            form.save()
+            return redirect('product_list')
+    else:
+        form = ProductForm(instance=product)
     return render(request, 'shop/add_product.html', {'form': form})
